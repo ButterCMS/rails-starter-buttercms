16e15fc511cf0a76369595498fc91a17498b6007
jirkamotejl <motejl.jirka@gmail.com>
2022-06-24 09:56:23 +0200

    component partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
-
   def index
-    render layout: "index"
+    page_type = params[:page_type] || 'landing-page'
+    slug = params[:slug] || 'landing-page-with-components'
+    @page_data = get_page(page_type, slug)
+    @menu_items = get_collection('navigation_menu_item')
+
+    render layout: page_type.to_s
   end
 
+  def get_collection(collection_name)
+    ButterCMS::Content.list(collection_name.to_s).first.data[1]
+  end
+
+  def get_page(page_type, slug)
+    ButterCMS::Page.get(page_type.to_s, slug.to_s).data.fields
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#index"
+  root 'pages#index'
+
+  get '/:page_type/:slug/', to: 'pages#index'
 end
