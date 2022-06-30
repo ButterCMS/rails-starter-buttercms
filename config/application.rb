fc2b58cd5e5486282c4c600858082958afe063cd
Jiří Motejl <74706221+jirkamotejl@users.noreply.github.com>
2022-06-30 16:05:02 +0200

    Heroku deployment (#1)
    * add platform to lockfile & remove sqlite
    
    * comment out database setting
    
    * get rid of active record and dependencies

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,20 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+
+%w[
+  active_model/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+].each do |railtie|
+  require railtie
+rescue LoadError
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
